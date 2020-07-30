b04f3c98db140c5eddb71da04aa18ddc1a57eca6
InvincibleDragon <InvincibleDragon@users.noreply.github.com>
2020-07-30 16:00:33 -0300

    first commit

---
+++ app/Models/Endereco.php
@@ -0,0 +1,20 @@
+<?php
+
+ namespace App\Models;
+
+ use Illuminate\Database\Eloquent\Model;
+
+ class Endereco extends Model{
+
+    protected $table = "enderecos";
+
+    protected $fillable = [
+        "cep",
+        "logradouro",
+        "numero",
+        "bairro",
+        "cidade",
+        "estado",
+    ];
+
+ }
